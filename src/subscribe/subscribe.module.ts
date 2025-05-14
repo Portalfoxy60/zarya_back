022d5bf4d77d5c182ec5c6d30fa028981115bb8b
Dmitrii Kreivald <dmitrii.kreivald@gmail.com>
2025-05-15 01:17:48 +0300

    DateService bugfix

--- src/subscribe/subscribe.module.ts
+++ src/subscribe/subscribe.module.ts
@@ -6,10 +6,11 @@ import { Subscribe } from './entities/subscribe.entity'
 import { User } from 'src/user/entities/user.entity'
 import { Product } from 'src/product/entities/product.entity'
 import { DateModule } from 'src/date/date.module'
+import { DateService } from 'src/date/date.service'
 
 @Module({
   imports: [TypeOrmModule.forFeature([Subscribe, User, Product]), DateModule],
   controllers: [SubscribeController],
-  providers: [SubscribeService],
+  providers: [SubscribeService, DateService],
 })
 export class SubscribeModule {}
